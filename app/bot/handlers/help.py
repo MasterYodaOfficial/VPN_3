4b09ffa18efd45bedad4b12249be33b490e7276b
masteyodaofficial <Evgrf86@inbox.ru>
2025-09-04 01:27:52 +0500

    V3.2 try ex help

--- app/bot/handlers/help.py
+++ app/bot/handlers/help.py
@@ -3,6 +3,7 @@ from app.bot.keyboards.inlines import help_menu_buttons, install_menu_buttons
 
 from aiogram.utils.i18n import gettext as _
 from app.core.config import settings
+from app.logger import logger
 
 async def help_command(message: Message):
     """Обработчик команды /help"""
@@ -14,19 +15,21 @@ async def help_command(message: Message):
 
 async def navigate_help_menu(call: CallbackQuery):
     action = call.data.split(":")[1]
-
-    if action == "install":
-        await call.message.edit_text(
-            text=_("choose_sys"),
-            reply_markup=install_menu_buttons()
-        )
-    elif action == "faq":
-        await call.message.edit_text(
-            text=_("help_faq_message").format(
-                owner=settings.OWNER_NAME
-            ),
-            reply_markup=install_menu_buttons()
-        )
+    try:
+        if action == "install":
+            await call.message.edit_text(
+                text=_("choose_sys"),
+                reply_markup=install_menu_buttons()
+            )
+        elif action == "faq":
+            await call.message.edit_text(
+                text=_("help_faq_message").format(
+                    owner=settings.OWNER_NAME
+                ),
+                reply_markup=install_menu_buttons()
+            )
+    except Exception as ex:
+        logger.debug(ex)
     await call.answer()
 
 
@@ -38,19 +41,21 @@ async def show_install_guide(call: CallbackQuery):
         "ios": _("help_ios_vless"),
         "desktop": _("help_desktop_vless")
     }
-
-    if platform in guides:
-        await call.message.edit_text(
-            text=guides[platform].format(
-                video=settings.INSTRUCTION_LINK
-            ),
-            reply_markup=install_menu_buttons(),
-            disable_web_page_preview=True
-        )
-    elif platform == "back_to_help":
-        await call.message.edit_text(
-            text=_("help_main_message"),
-            reply_markup=help_menu_buttons(),
-            disable_web_page_preview=True
-        )
+    try:
+        if platform in guides:
+            await call.message.edit_text(
+                text=guides[platform].format(
+                    video=settings.INSTRUCTION_LINK
+                ),
+                reply_markup=install_menu_buttons(),
+                disable_web_page_preview=True
+            )
+        elif platform == "back_to_help":
+            await call.message.edit_text(
+                text=_("help_main_message"),
+                reply_markup=help_menu_buttons(),
+                disable_web_page_preview=True
+            )
+    except Exception as ex:
+        logger.debug(ex)
     await call.answer()
